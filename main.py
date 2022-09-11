b34a5310abab58531b7f83467d3314096573cf33
EshaanBansal09 <83131599+EshaanBansal09@users.noreply.github.com>
2022-09-11 22:02:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+print('Welcome to the Band Name Generator!! ')
+adjective = input('Please enter the adjective you would like to define yourself with: \n')
+food = input('Please enter the name of  your favourite food: \n')
+final = adjective + " " + food
+print("Your Band Name could be "+final)
